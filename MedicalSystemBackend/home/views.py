b380e9d0d7fecaed59424b9d6c07f66b2f2f6f70
Siddharth-Nama <siddharthnama2604@gmail.com>
2025-01-11 17:08:06 +0530

    Added some new feature

--- MedicalSystemBackend/home/views.py
+++ MedicalSystemBackend/home/views.py
@@ -49,12 +49,9 @@ class MedicineDistributionViewSet(viewsets.ModelViewSet):
     serializer_class = MedicineDistributionSerializer
 
     def get_queryset(self):
-        """
-        Override to allow filtering by date range and roll number.
-        """
+        
         queryset = super().get_queryset()
 
-        # Get query parameters
         start_date = self.request.query_params.get("start_date")
         end_date = self.request.query_params.get("end_date")
         roll_number = self.request.query_params.get("roll_number")
@@ -63,7 +60,7 @@ class MedicineDistributionViewSet(viewsets.ModelViewSet):
         if start_date and end_date:
             queryset = queryset.filter(date__range=[start_date, end_date])
 
-        # Filter by roll number (if provided)
+        # Filter by roll number
         if roll_number:
             queryset = queryset.filter(student__roll_number__icontains=roll_number)
 
